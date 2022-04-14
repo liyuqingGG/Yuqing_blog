e15a0082c560019bdad5ac00e2ad5d4b879e1fc9
liyuqingGG <liyuqinggg@foxmail.com>
2022-04-14 15:15:06 +0800

    新增django笔记_01

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -55,8 +55,8 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
       //   ],
       // },
       {
-        text: '页面',
-        link: '/ui/',
+        text: '前端',
+        link: '/web',
         items: [
           { text: '前端文章', link: '/pages/a61298/' },
           { text: 'HTML', link: '/pages/8309a5b876fc95e3/' },
@@ -68,7 +68,8 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
         link: '/technology/',
         items: [
           { text: 'git相关', link: '/pages/3ac5a6/' },
-          { text: 'mysql相关', link: '/pages/1ba760/' }
+          { text: 'mysql相关', link: '/pages/1ba760/' },
+          { text:'python', link:'/pages/f95cf0/'}
         ],
       },
       {
